420db1ec008bca0362091f1e13f256012021260f
M-Usman10 <muhammadusman1.me@gmail.com>
2019-08-11 20:38:41 +0500

    Modified Outlier detector

--- preprocessing/clean.py
+++ preprocessing/clean.py
@@ -5,8 +5,9 @@ def remove_outliers(data_list, idx, lim=3, attr_thresh=20):
     data = data_list[idx]
     mean = np.mean(data, axis=0)
     std = np.mean(data, axis=0)
-    to_keep = np.where(~(np.sum((data <= (mean - lim * std)) | (data >= (mean + lim * std)), axis=1) > attr_thresh))[0]
+    to_rem = (np.sum((data <= (mean - lim * std)) | (data >= (mean + lim * std)), axis=1) > attr_thresh)
+    to_keep = np.where(~to_rem)[0]
     res = []
     for elem in data_list:
         res.append(elem[to_keep])
-    return res
+    return res, np.where(to_rem)
