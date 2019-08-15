938f391222aa136f78eca4bf7efaf026df1c4dc7
M-Usman10 <muhammadusman1.me@gmail.com>
2019-08-15 15:45:22 +0500

    Save images to a directory

--- preprocessing/savers.py
+++ preprocessing/savers.py
@@ -11,6 +11,4 @@ def save_images(imgs, outDir, names=None):
     if not os.path.isdir(outDir):
         os.mkdir(outDir)
     for idx, img in enumerate(imgs):
-        print(outDir, names[idx])
-        print(os.path.join(outDir, names[idx]))
-        cv2.imwrite(img, os.path.join(outDir, names[idx]))
+        cv2.imwrite(os.path.join(outDir, names[idx]), img)
