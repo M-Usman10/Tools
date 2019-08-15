f97256fd581bc7bd56fe1c527330ee547a6d35e5
M-Usman10 <muhammadusman1.me@gmail.com>
2019-08-15 10:24:26 +0500

    Image reader added

--- preprocessing/readers.py
+++ preprocessing/readers.py
@@ -1,8 +1,30 @@
+import glob
+import os
+
+import cv2
 import h5py
 import numpy as np
 
 
-def readHdf5(inputPath,dataset="images"):
+def read_hdf5(inputPath, dataset="images"):
     files = h5py.File(inputPath, 'r')
     return files[dataset][:]
 
+
+def read_images(inputPath, preprocess=None, format=''):
+    """
+
+    :param inputPath: Path of directory where images are placed
+    :param preprocess: Functor for preprocessing
+    :param format: '.jpg' etc.
+    :return: Numpy array of images
+    """
+    images = []
+    image_paths = glob.glob(os.path.join(inputPath, '*' + format))
+    if preprocess is None:
+        for path in image_paths:
+            images.append(cv2.imread(path))
+    else:
+        for path in image_paths:
+            images.append(preprocess(cv2.imread(path)))
+    return np.array(images)
