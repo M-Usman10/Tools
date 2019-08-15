b6645e8a89e4fa39c312aed63436695cfeb6db5c
M-Usman10 <muhammadusman1.me@gmail.com>
2019-08-15 15:00:50 +0500

    Save images to a directory

--- preprocessing/savers.py
+++ preprocessing/savers.py
@@ -4,10 +4,10 @@ import cv2
 import numpy as np
 
 
-def save_imgs(imgs, outDir, names=None):
+def save_images(imgs, outDir, names=None):
     if names is None:
         names = np.array(range(len(imgs)))
-        names = np.core.defchararray.add(names, '.jpg')
+        names = np.core.defchararray.add(names, np.array(['.jpg'] * len(imgs)))
     if not os.path.isdir(outDir):
         os.mkdir(outDir)
     for idx, img in enumerate(imgs):
