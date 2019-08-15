4a54d7d99339c974c52d15a29f20e301cd13bc48
M-Usman10 <muhammadusman1.me@gmail.com>
2019-08-15 15:38:04 +0500

    Save images to a directory

--- preprocessing/savers.py
+++ preprocessing/savers.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def save_images(imgs, outDir, names=None):
     if names is None:
-        names = np.array(range(len(imgs)))
+        names = np.array(range(len(imgs))).astype(str)
         names = np.core.defchararray.add(names, np.array(['.jpg'] * len(imgs)))
     if not os.path.isdir(outDir):
         os.mkdir(outDir)
