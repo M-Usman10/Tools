7415c03c7f357fcc7df243841c42c63cec1bbd25
M-Usman10 <muhammadusman1.me@gmail.com>
2020-03-20 15:58:12 +0500

    Exp weighted avg to estimate accurate time in enumerate

--- others/time.py
+++ others/time.py
@@ -1,7 +1,8 @@
 import timeit
-def enumerate_t(iter,total_len=None):
+def enumerate_t(iter,total_len=None,beta = 0.97):
     count =0
     prev = timeit.default_timer()
+    diff_avg = 0
     for item in iter:
         count+=1
         current_time = timeit.default_timer()
@@ -9,4 +10,5 @@ def enumerate_t(iter,total_len=None):
         prev = current_time
         if total_len is not None:
             diff = (total_len-count)*diff
+            diff_avg = beta* diff_avg + (1-beta)*diff
         yield count, diff, item
